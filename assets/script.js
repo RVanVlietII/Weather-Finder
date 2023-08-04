c1a270601d50fb27666c82545e0e1bfd718d99a9
RVanVlietII <rcvanvlietii@gmail.com>
2023-08-04 00:32:12 -0400

    fetch fix for obtaining lat and lon

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
-let resultsContainer = $(".resultsContainer");
+let resultsContainer = $("#resultsContainer");
 let searchBtn = $("#searchButton");
 //let encodedCityName = encodeURIComponent(cityName);
 let cityName;
 
-//let latitudeEl = {"$lat"};
-//let longitudeEl = {"$lon"};
-let apiKey = "aebb3a771712c2ba6165d4a2e0e458a5";
+let latitudeEl;
+let longitudeEl;
+let apiKey = "d9dc192660aded560ce44b031b481711";
 
-//let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitudeEl + "&lon=" + longitudeEl + "&appid=" + apiKey;
+// let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat={latitudeEl}&lon={longitudeEl}&appid={apiKey}`;
 //let obtainCoordinates = "https://api.openweathermap.org/geo/1.0/direct?q=" + encodedCityName + "&limit=2&appid=" + apiKey;
+let weatherURL;
 
 function getWeather (cityName) {
     let encodedCityName = encodeURIComponent(cityName);
-    let obtainCoordinates = `https://api.openweathermap.org/geo/1.0/direct?q=${encodedCityName}&limit=2&appid=${apiKey}`;
+    let obtainCoordinates = `https://api.openweathermap.org/geo/1.0/direct?q=${encodedCityName}&limit=1&appid=${apiKey}`;
 
 
     fetch(obtainCoordinates, {
@@ -23,10 +24,41 @@ function getWeather (cityName) {
         })
         .then(function (data) {
             console.log(data);
+            // if (!Array.isArray(data) || data.length === 0 || !data[0].lat) {
+            //     console.error("City not found or invalid response from the coordinates API.");
+            //     return;
+
+            // }
+        })
+        // .catch(function(error) {
+        //     console.error("Error fetching weather data:", error);
+        // })
+
+    
+    
+    
+    .then(function (data) {
+
+        console.log(data);
+    
+        latitudeEl = data[0].lat;
+        longitudeEl = data[0].lon;
+
+    let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat={latitudeEl}&lon={longitudeEl}&appid={apiKey}`;
+
+    fetch(weatherURL, {
+        method: 'GET'
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
         })
         .catch(function(error) {
-            console.error("Error fetching weather data:", error);
+            console.error("Error fetching weather data from coordinates", error);
         })
+    })
 };
 
 
