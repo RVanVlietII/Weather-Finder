520c083ab59d6d8b820c048d6496c32cbcbbb767
RVanVlietII <rcvanvlietii@gmail.com>
2023-08-04 09:04:14 -0400

    fetch changes

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,12 @@
 let resultsContainer = $("#resultsContainer");
 let searchBtn = $("#searchButton");
-//let encodedCityName = encodeURIComponent(cityName);
+
 let cityName;
 
 let latitudeEl;
 let longitudeEl;
 let apiKey = "d9dc192660aded560ce44b031b481711";
 
-// let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat={latitudeEl}&lon={longitudeEl}&appid={apiKey}`;
-//let obtainCoordinates = "https://api.openweathermap.org/geo/1.0/direct?q=" + encodedCityName + "&limit=2&appid=" + apiKey;
 let weatherURL;
 
 function getWeather (cityName) {
@@ -23,46 +21,77 @@ function getWeather (cityName) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            // if (!Array.isArray(data) || data.length === 0 || !data[0].lat) {
-            //     console.error("City not found or invalid response from the coordinates API.");
-            //     return;
-
-            // }
+            if (data && data.length > 0) {
+                const latitudeEl = data[0].lat;
+                const longitudeEl = data[0].lon;
+                
+                console.log('Latitude:', latitudeEl);
+                console.log('Longitude:', longitudeEl);
+            } else {
+                console.error('Data array is empty or undefined.');
+            }
         })
-        // .catch(function(error) {
-        //     console.error("Error fetching weather data:", error);
-        // })
 
-    
-    
-    
-    .then(function (data) {
+        const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitudeEl}&lon=${longitudeEl}&appid=${apiKey}`;
 
-        console.log(data);
-    
-        latitudeEl = data[0].lat;
-        longitudeEl = data[0].lon;
-
-    let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat={latitudeEl}&lon={longitudeEl}&appid={apiKey}`;
-
-    fetch(weatherURL, {
-        method: 'GET'
-    })
-        .then(function (response) {
-            return response.json();
+        fetch(weatherURL, {
+            method: 'GET'
         })
-        .then(function (data) {
-            console.log(data);
-        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function(error) {
+                console.error("Error fetching weather data from coordinates", error);
+            })
         .catch(function(error) {
-            console.error("Error fetching weather data from coordinates", error);
+            console.error("Error fetching weather data:", error);
         })
-    })
-};
+
+    
+
+    // const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitudeEl}&lon=${longitudeEl}&appid=${apiKey}`;
+
+    // fetch(weatherURL, {
+    //     method: 'GET'
+    // })
+    //     .then(function (response) {
+    //         return response.json();
+    //     })
+    //     .then(function (data) {
+    //         console.log(data);
+    //     })
+    //     .catch(function(error) {
+    //         console.error("Error fetching weather data from coordinates", error);
+    //     })
+    };
 
 
 searchBtn.on("click", function() {
     cityName = $("#cityInput").val();
     getWeather(cityName);
 });
+
+
+// Example code to fetch data from OpenWeatherMap API
+// fetch('your_openweathermap_api_url_here')
+//   .then(response => response.json())
+//   .then(data => {
+//     // Check if the data array is not empty
+//     if (data && data.length > 0) {
+//       // Access latitude and longitude from the first element of the data array
+//       const latitudeEl = data[0].lat;
+//       const longitudeEl = data[0].lon;
+
+//       // Do something with the latitude and longitude values
+//       console.log('Latitude:', latitudeEl);
+//       console.log('Longitude:', longitudeEl);
+//     } else {
+//       console.error('Data array is empty or undefined.');
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data from OpenWeatherMap API:', error);
+//   });
